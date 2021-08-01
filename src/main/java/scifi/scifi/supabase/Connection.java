3ff1b7b3a78dd8a0fc2d58d2538466fcc1c02daa
Brady Bellini <bbellini@pm.me>
2021-07-31 22:48:23 -0700

    Update Connection.java

--- src/main/java/scifi/scifi/supabase/Connection.java
+++ src/main/java/scifi/scifi/supabase/Connection.java
@@ -1,13 +1,9 @@
 package scifi.scifi.supabase;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-
 import java.net.http.HttpClient;
 
 public class Connection {
 
     private HttpClient client;
-    private Request request;
 
 }
