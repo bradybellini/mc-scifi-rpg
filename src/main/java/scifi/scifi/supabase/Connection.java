e046ea5a0ba6ca302d5361b01c9e5ca306ecfea3
Brady Bellini <bbellini@pm.me>
2021-08-01 00:54:45 -0700

    Update Connection.java
    created basics for api connections, will be cleaned up with more robust classes for using elsewhere

--- src/main/java/scifi/scifi/supabase/Connection.java
+++ src/main/java/scifi/scifi/supabase/Connection.java
@@ -1,9 +1,31 @@
 package scifi.scifi.supabase;
 
+import scifi.scifi.McScifi;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Connection {
 
-    private HttpClient client;
+    private HttpClient client = HttpClient.newHttpClient();
+
+
+    public HttpResponse<String> getRequest(String query) throws IOException, InterruptedException {
+        var request = HttpRequest.newBuilder(URI.create(query))
+                .header("apikey", McScifi.getPlugin(McScifi.class).getConfig().getString("supabaseprivatekey"))
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    }
+
+
 
 }
