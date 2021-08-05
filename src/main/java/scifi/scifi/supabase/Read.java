a877db85db11e476d12c0810f8500e45c9c79f02
Brady Bellini <bbellini@pm.me>
2021-08-04 23:24:15 -0700

    added getallplayers methods
    this is still a WIP

--- src/main/java/scifi/scifi/supabase/Read.java
+++ src/main/java/scifi/scifi/supabase/Read.java
@@ -1,4 +1,13 @@
 package scifi.scifi.supabase;
 
+import java.io.IOException;
+
 public class Read {
+
+    private Connection conn;
+
+    public String getAllPlayers() throws IOException, InterruptedException {
+        return conn.getRequest("player?select=*").body();
+    }
+
 }
