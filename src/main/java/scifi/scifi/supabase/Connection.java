74a86ff090993f5f0e6a2f5dd5d7d818438a5ecb
Brady Bellini <bbellini@pm.me>
2021-08-04 23:23:57 -0700

    added get and post request methods
    see todo for making things more efficient. Added get and post request, still possibly need put and delete.

--- src/main/java/scifi/scifi/supabase/Connection.java
+++ src/main/java/scifi/scifi/supabase/Connection.java
@@ -3,29 +3,50 @@ package scifi.scifi.supabase;
 import scifi.scifi.McScifi;
 
 import java.io.IOException;
-import java.net.Authenticator;
 import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 public class Connection {
 
     private HttpClient client = HttpClient.newHttpClient();
+    private String baseURL = "https://ypallacrxhalhdrkcbug.supabase.co/rest/v1/";
 
 
-    public HttpResponse<String> getRequest(String query) throws IOException, InterruptedException {
-        var request = HttpRequest.newBuilder(URI.create(query))
-                .header("apikey", McScifi.getPlugin(McScifi.class).getConfig().getString("supabaseprivatekey"))
-                .build();
+    public HttpRequest createRequest(HttpRequest.Builder builder, String params) throws IOException, InterruptedException {
 
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
+        builder.header("apikey", McScifi.getPlugin(McScifi.class).getConfig().getString("supabaseprivatekey"));
+        builder.uri(URI.create(baseURL + params));
+        HttpRequest request = builder.build();
 
+        return request;
     }
 
+    /*
+     TODO there has to be a more efficient way to do this for the different request methods
+     look into making a Builder builder (lol) for creating the builder? I thought about it but was not
+     really coming up with anything that I felt would work. There is a lot of repeated code that could
+     be moved to some catch all methods or something. idk this just seems inefficient but it works for
+     now and can be cleaned up at a later  time.
+    */
 
+    public HttpResponse<String> getRequest(String params) throws IOException, InterruptedException {
+
+        HttpRequest.Builder builder = HttpRequest.newBuilder();
+        HttpRequest request = this.createRequest(builder, params);
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> postRequest(String params, String data) throws IOException, InterruptedException {
+
+        HttpRequest.Builder builder = HttpRequest.newBuilder();
+        builder.POST(HttpRequest.BodyPublishers.ofString(data));
+        HttpRequest request = this.createRequest(builder, params);
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
 
 }
